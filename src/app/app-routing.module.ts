ef2f994f7a8f47aa6a9ca336ec885dd7463a46b9
sheetus <47025393+sheetus@users.noreply.github.com>
2023-02-10 15:07:41 +0200

    Login and Register Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
-import { NgModule } from '@angular/core';
+import { CartComponent } from './cart/cart.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductcardComponent } from './productcard/productcard.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthGuardGuard } from './auth-guard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:ProductListComponent,
+  },
+  {
+    path:'product-card',
+    component:ProductcardComponent,
+  } , {
+    path:'product-details/:id',// /:id to make it dynamic 
+    component:ProductDetailsComponent,
+  } ,  {
+    path:'register',
+    component:RegisterComponent,
+  } , {
+    path:'login',
+    component:LoginComponent,
+  }
+  ,{
+    path:'cart/:id',
+    component:CartComponent,
+    canActivate:[AuthGuardGuard],
+  }
+  , {path:'**',
+  component:NotfoundComponent,
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductcardComponent } from './productcard/productcard.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { CartComponent } from './cart/cart.component';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductcardComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    RegisterComponent,
+    LoginComponent,
+    NotfoundComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
